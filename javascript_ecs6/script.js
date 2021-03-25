fcfd6f04e7919e8b7e2f5a6b4802d5c1c1b6a7ed
Your Name <you@example.com>
2021-03-25 17:35:04 -0300

    iniciando em JS

---
+++ javascript_ecs6/script.js
@@ -0,0 +1,25 @@
+//var serve para armazenar dados na memoria ou seja evitar repetições
+/*
+var nome = 'Junior Rijo'
+
+var idade = '26 anos'
+*/
+//condicionais
+var possuiGraduacao = 0
+var possuioDoutorado = 0
+/* condicao simples
+if(possuiGraduacao){
+    console.log('é verdade esse bilhete')
+} else{
+    console.log('Falso')
+}
+*/
+/*
+if(possuiGraduacao){
+    console.log('Verdadeiro')
+} else if(possuioDoutorado){
+    console.log('Falso')
+} else{
+    console.log('Olámundo')
+}
+*/
